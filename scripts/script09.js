24c1c696ed6260a1fc5bf144a1c14cc6747279c7
uailucasSilva <107223850+uailucasSilva@users.noreply.github.com>
2022-08-21 22:48:45 -0300

    Add files via upload

--- scripts/script09.js
+++ scripts/script09.js
@@ -1,13 +1,16 @@
-texto = prompt("Informe um texto:");
+textoIn = prompt("Informe um texto:");
+texto = textoIn.replace(/\s/g, '');
 tamanho = texto.length;
 tamanhoAux = tamanho;
 count = 0;
 for (i = 0; i < tamanho; i++) {
-    if (((texto.charAt(i)).toUpperCase()).trim == ((texto.charAt(tamanhoAux - 1)).toUpperCase()).trim) {
+
+    if ((texto.charAt(i)).toUpperCase() == (texto.charAt(tamanhoAux - 1).toUpperCase()).trim()) {
         count++;
     }
     tamanhoAux--;
 }
+
 if (count == tamanho) {
     alert("O texto informado é um palíndromo");
 }
